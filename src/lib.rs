4d18e6054e5f0923c3f5bce836405309b5dbeab9
Andrey Kuznetsov <fear@loathing.in>
2023-11-13 17:28:43 +0400

    feat: CoinExt and get_denom

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,36 @@
-include!("gen/mod.rs");
+// include!("gen/mod.rs");
+mod gen;
+pub use gen::*;
 
 pub type ClientChannel = tonic::transport::Channel;
 pub use tonic;
+
+use cosmos::base::v1beta1::Coin;
+
+pub trait CoinExt {
+    fn get_denom(&self, denom: impl Into<String>) -> Option<&Coin>;
+}
+
+impl CoinExt for Option<Coin> {
+    fn get_denom(&self, denom: impl Into<String>) -> Option<&Coin> {
+        match self {
+            coin @ Some(Coin { denom: d, .. }) if d == &denom.into() => coin.as_ref(),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn coin_ext() {
+        let coin = Coin {
+            denom: "aISLM".to_string(),
+            amount: "0".to_string(),
+        };
+
+        assert!(Some(coin).get_denom("aISLM").is_some())
+    }
+}
